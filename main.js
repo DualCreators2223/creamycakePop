a8bffeb2144db1932dcb8ce086121b2cd61d293e
DualCreators2223 <121956588+DualCreators2223@users.noreply.github.com>
2023-03-05 13:22:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const sr =  ScrollReveal({
+    distance : '45px',
+    duration : 1000,
+    reset : true,
+})
+sr.reveal('.logo',{delay:350,origin:'top'})
+// sr.reveal('.myimageabout',{delay:350,origin:'left'})
+// sr.reveal('.about-text-content',{delay:150,origin:'left'})
+// sr.reveal('.quality-content',{delay:350,origin:'top'})
+// sr.reveal('.quality-content qulity-image',{delay:350,origin:'top'})
+// sr.reveal('.gallery-image',{delay:350,origin:'top'})
+// sr.reveal('.menu-food-content',{delay:350,origin:'right'})
+// sr.reveal('.main-footer',{delay:350,origin:'left'})
